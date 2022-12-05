aaae568b42184e9bb31509e5b6f8553df1ce518c
wwh <wangwen135@163.com>
2022-12-05 23:21:39 +0800

    video play

--- src/main/java/com/wwh/home/center/controller/CameraVideoController.java
+++ src/main/java/com/wwh/home/center/controller/CameraVideoController.java
@@ -6,8 +6,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -107,8 +110,41 @@ public class CameraVideoController {
         return Arrays.stream(minutes).map(x -> x.substring(0, 2)).collect(Collectors.toList());
     }
 
+
+    /**
+     * 分配获取视频
+     *
+     * @param code
+     * @param date
+     * @param hour
+     * @param minute
+     * @return
+     */
     @GetMapping("/getVideo")
-    public void getVideoStream(@RequestParam String code, @RequestParam String date, @RequestParam String hour, @RequestParam String minute, HttpServletResponse response) {
+    public ResponseEntity getVideoStream(@RequestParam String code, @RequestParam String date, @RequestParam String hour, @RequestParam String minute) {
+
+        File baseDir = new File(videoBasePath);
+        File videoDir = new File(baseDir, code);
+        File ymdhDir = new File(videoDir, date + hour);
+        File[] videoFile = ymdhDir.listFiles((dir, name) -> name.startsWith(minute));
+        if (videoFile == null || videoFile.length == 0) {
+            log.warn("视频文件没有找到，路径：{}，分钟：{}", ymdhDir.getAbsolutePath(), minute);
+            return new ResponseEntity("video file not found", HttpStatus.NOT_FOUND);
+        }
+        //每分钟只有一个文件
+        File file = videoFile[0];
+        String fileName = date + "-" + hour + "#" + minute + ".mp4";
+        //这个写法是支持断点续传的
+        //Accept-Ranges: bytes
+        //Content-Range: bytes 6258688-6792415/6792416
+        return ResponseEntity.ok().contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName)
+                .body(new FileSystemResource(file));
+    }
+
+
+    //这个写法被淘汰了
+    public void getVideoStream2(@RequestParam String code, @RequestParam String date, @RequestParam String hour, @RequestParam String minute, HttpServletResponse response) {
 
         try {
             File baseDir = new File(videoBasePath);
@@ -125,6 +161,8 @@ public class CameraVideoController {
             File file = videoFile[0];
             response.setContentType(MediaType.APPLICATION_OCTET_STREAM_VALUE);
             response.setHeader("Content-Disposition", "attachment; filename=" + file.getName());
+            response.setContentLength((int) file.length());
+            //response.setHeader("Accept-Ranges", "bytes"); 不支持
             InputStream iStream = new FileInputStream(file);
             IOUtils.copy(iStream, response.getOutputStream());
             response.flushBuffer();
